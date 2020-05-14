c9f53893ad5dcf14eb72c707e97f43c312d20ba6
anna-xing <anna.xing.22@gmail.com>
2020-05-14 19:51:27 -0400

    opened actual site

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Importing nav and footer
 $.get("nav.html", function (data) {
     $("#nav-placeholder").replaceWith(data);
 });
@@ -5,3 +6,31 @@ $.get("nav.html", function (data) {
 $.get("footer.html", function (data) {
     $("#footer-placeholder").replaceWith(data);
 });
+
+// Project tags hover animation
+let projectTagsWidgets = document.querySelectorAll(".project-tags-widget");
+projectTagsWidgets.forEach(function (widget) {
+    widget.addEventListener("mouseover", function () {
+        widget.classList.add("open");
+    });
+    widget.addEventListener("mouseleave", function () {
+        widget.classList.remove("open");
+    });
+});
+
+// Changing active nav tab
+window.onload = function () {
+    $("#nav-projects").removeClass("active");
+    $("#nav-about").removeClass("active");
+    $("nav-oddsends").removeClass("active");
+
+    let path = window.location.pathname;
+
+    if (path === "/about.html") {
+        $("#nav-about").addClass("active");
+    } else if (path === "/oddsends.html") {
+        $("#nav-oddsends").addClass("active");
+    } else {
+        $("#nav-projects").addClass("active");
+    }
+}
