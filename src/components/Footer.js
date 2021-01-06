ec650045f8831b8de7571403067e17ef28acb4bc
anna-xing <anna.xing.22@gmail.com>
2021-01-06 17:52:39 -0500

    Created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faMediumM, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+const IconContainer = styled.div`
+    text-align: center;
+`;
+
 const FooterText = styled.p`
     font-family: ${({theme}) => theme.fontFamily.body};
-    font-size: ${({theme}) => theme.fontSize.small};
+    font-size: ${({theme}) => theme.fontSize.xs};
     font-weight: normal;
     color: ${({theme}) => theme.color.textSecondary};
 `;
@@ -13,10 +21,21 @@ const FooterContainer = styled.footer`
     margin: ${({theme}) => theme.padding.pageVertical} ${({theme}) => theme.padding.pageHorizontal};
 `;
 
+const StyledIcon = styled(FontAwesomeIcon)`
+    margin: 0 12px;
+    color: ${({theme}) => theme.color.textPrimary};
+`;
+
 export const Footer = () => {
     return (
         <FooterContainer>
-            <FooterText>Made with ♥ by Anna Xing</FooterText>
+            <IconContainer>
+                <StyledIcon icon={faLinkedinIn} size='lg' />
+                <StyledIcon icon={faGithub} size='lg' />
+                <StyledIcon icon={faMediumM} size='lg' />
+                <StyledIcon icon={faEnvelope} size='lg' />
+            </IconContainer>
+            <FooterText>Made with <FontAwesomeIcon icon={faHeart} /> by Anna Xing</FooterText>
         </FooterContainer>
     );
 }
