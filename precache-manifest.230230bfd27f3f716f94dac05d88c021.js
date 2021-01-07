5fac60a29bd7f2c34db9c27ba452491611548e83
anna-xing <anna.xing.22@gmail.com>
2021-01-06 23:59:30 -0500

    Updates

---
+++ precache-manifest.230230bfd27f3f716f94dac05d88c021.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554809434532cce3a8db231afa340aea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c24dbf2c4bcdc43158e",
+    "url": "/static/js/2.629db480.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.629db480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d63b021178039173bb",
+    "url": "/static/js/main.8f1fdbce.chunk.js"
+  },
+  {
+    "revision": "ec37339358264786a889",
+    "url": "/static/js/runtime-main.2d3133e2.js"
+  },
+  {
+    "revision": "4e6c077167575762da13b94c3954e77e",
+    "url": "/static/media/SofiaProBlack.4e6c0771.woff"
+  },
+  {
+    "revision": "55ee1f1f2590f23b470a08110a7b7faa",
+    "url": "/static/media/SofiaProMedium-Italic.55ee1f1f.woff"
+  },
+  {
+    "revision": "d356bfed476880d23309cba2b1caca25",
+    "url": "/static/media/SofiaProMedium.d356bfed.woff"
+  },
+  {
+    "revision": "e689165714e2644f03bba4f9ba98a8dd",
+    "url": "/static/media/bitwheel.e6891657.jpg"
+  },
+  {
+    "revision": "0feae5419c09da53519643b9b339bd29",
+    "url": "/static/media/hurr-evac.0feae541.jpg"
+  },
+  {
+    "revision": "e46f10ca8e6a896b38df2ae0fc43e59e",
+    "url": "/static/media/project-pink.e46f10ca.jpg"
+  },
+  {
+    "revision": "06a4439a4faa3eb3bd503e3587689048",
+    "url": "/static/media/ticker-extension.06a4439a.jpg"
+  }
+]);
