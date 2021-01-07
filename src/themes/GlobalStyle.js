c59a60ec8ab5d95aaca08ef63d0d2d488bb1258e
anna-xing <anna.xing.22@gmail.com>
2021-01-07 15:50:05 -0500

    Made theme transitions smooth

--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -29,6 +29,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
         text-align: center;
         background-color: ${(props) => props.theme.color.backgroundPrimary};
+        transition: all 300ms linear;
     }
 
     a {
