8585cad87cbecef6612a5c92642e6e1080533662
anna-xing <anna.xing.22@gmail.com>
2021-01-07 18:10:38 -0500

    Added under construction msg to Play pg

--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTools } from '@fortawesome/free-solid-svg-icons';
+
 import {
     BannerText,
-    HeadingText,
     BodyText,
-    TagContainer,
-    TagText,
-    Tag,
-    SingleCardContainer, 
-    SingleTextCard,
-    SingleCardsGrid,
     BannerContainer,
 } from './../components';
 
@@ -18,57 +14,12 @@ export const PlayPage = ({theme}) => {
         <React.Fragment>
             <BannerContainer>
                 <BannerText>Odds & Ends</BannerText>
-                <BodyText>An assortment of writing, digital art, and paintings.</BodyText>
+                <BodyText>This page is currently under construction.</BodyText>
             </BannerContainer>
-            <SingleCardsGrid>
-                <a href="https://anna-xing.medium.com/a-brief-overview-of-big-tech-illustration-flat-design-corporate-memphis-and-alegria-a9b54a35c6b1" target="_blank" rel="noopener noreferrer">
-                    <SingleCardContainer height={theme.cardSize.writingHeight}>
-                        <SingleTextCard>
-                            <TagContainer>
-                                <Tag><TagText>Writing</TagText></Tag>
-                            </TagContainer>
-                            <HeadingText>A Brief Overview of Big Tech Illustration</HeadingText>
-                            <BodyText>Flat Design, Corporate Memphis, and Alegria</BodyText>
-                        </SingleTextCard>
-                    </SingleCardContainer>
-                </a>
-                <a href="https://qwhery.com/collin-county-texas-achieves-record-breaking-turnout-with-location-based-outreach-tools/" target="_blank" rel="noopener noreferrer">
-                    <SingleCardContainer height={theme.cardSize.writingHeight}>
-                        <SingleTextCard>
-                            <TagContainer>
-                                <Tag><TagText>Writing</TagText></Tag>
-                                <Tag><TagText>Qwhery</TagText></Tag>
-                            </TagContainer>
-                            <HeadingText>Collin County, Texas Achieves Record-Breaking Voter Turnout</HeadingText>
-                            <BodyText>The Role of Location-Based Outreach Tools in its Election</BodyText>
-                        </SingleTextCard>
-                    </SingleCardContainer>
-                </a>
-                <a href="https://qwhery.com/introducing-the-qwhery-cloud/" rel='noopener noreferrer'>
-                    <SingleCardContainer height={theme.cardSize.writingHeight}>
-                        <SingleTextCard>
-                            <TagContainer>
-                                <Tag><TagText>Writing</TagText></Tag>
-                                <Tag><TagText>Qwhery</TagText></Tag>
-                            </TagContainer>
-                            <HeadingText>Engaging Communities with Voice Tech and Location Intelligence</HeadingText>
-                        </SingleTextCard>
-                    </SingleCardContainer>
-                </a>
-                <a href="https://qwhery.com/q11-connecting-municipalities-directly-with-residents/" rel='noopener noreferrer'>
-                    <SingleCardContainer height={theme.cardSize.writingHeight}>
-                        <SingleTextCard>
-                            <TagContainer>
-                                <Tag><TagText>Writing</TagText></Tag>
-                                <Tag><TagText>Qwhery</TagText></Tag>
-                            </TagContainer>
-                            <HeadingText>Q11: Connecting Municipalities Directly with Residents</HeadingText>
-                            <BodyText>Filling the Gaps in Municipal Information Systems</BodyText>
-                        </SingleTextCard>
-                    </SingleCardContainer>
-
-                </a>
-            </SingleCardsGrid>
+            <FontAwesomeIcon icon={faTools} size="6x" color={theme.color.textPrimary} />
+            <br />
+            <br />
+            <br />
         </React.Fragment>
     );
 };
