a676518e145e60a585674f3451c22749e618ddb7
anna-xing <anna.xing.22@gmail.com>
2021-01-08 09:30:35 -0500

    Removed ColorBlock file and added svg blobs

---
+++ src/assets/img/blobs/index.js
@@ -0,0 +1,13 @@
+import blob1 from './blob1.svg';
+import blob2 from './blob2.svg';
+import blob3 from './blob3.svg';
+import blob4 from './blob4.svg';
+import blob5 from './blob5.svg';
+
+export {
+    blob1,
+    blob2,
+    blob3,
+    blob4,
+    blob5,
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export * from './Nav';
 export * from './PageContainer';
 export * from './TextType';
 export * from './Card';
-export * from './ColorBlock';
 export * from './Tag';
 export * from './BannerContainer';
 export * from './TextContainer';
