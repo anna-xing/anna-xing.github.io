f253d166bd512b9831b15103ef2f12770047b336
anna-xing <anna.xing.22@gmail.com>
2021-05-15 00:25:51 -0400

    Update About page with DFG role

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -22,16 +22,20 @@ export const AboutPage = ({theme}) => {
             </BannerContainer>
             <TextContainer>
                 <BodyText>
-                    I'm a second-year BCS student at the University of Waterloo.
-	            This spring, I'll be working as a Full-Stack Portfolio Analytics Developer at TD.
-                    In the past, I've also been a Web Developer for UW's Computer Science Computing Facility,
-                    where I worked on applications supporting faculty and students. 
+                    I'm a second-year BCS student at the University of Waterloo. 
+	    	    I'm also currently a Portfolio Analytics Developer Intern at TD. 
+	            In my spare time, I volunteer as a Software Developer and Product Designer at Develop for Good, 
+	            where I'm building a full-stack web app for the educational nonprofit Classrooms Without Borders!
+	    	</BodyText>
+	    	<BodyText>
+                    In the past, I've also been a Web Application Developer for UW's Computer Science Computing Facility,
+	            where I worked on applications supporting faculty and students. 
 	            I'm currently seeking opportunities for Winter 2022 in software development and UI/UX.
                 </BodyText>
                 <BodyText>
                     I'm passionate about human-centered design, which is why I'm pursuing a Human-Computer 
                     Interaction specialization and Cognitive Science minor within my degree. I love applying 
-	            interdisciplinary knowledge to design and build products with the potential to do social good.
+	            interdisciplinary knowledge to design and build products for social good.
                 </BodyText>
                 <BodyText>
                     To chat about anything from software to Spanish basilicas, feel free to connect with 
@@ -43,12 +47,12 @@ export const AboutPage = ({theme}) => {
                     When I'm not tinkering with dev tools, you can find me:
                     <Typical
                         steps={[
-                            ' making art.', theme.transition.typical,
                             ' jamming on the ukulele.', theme.transition.typical,
+                            ' making art.', theme.transition.typical,
                             ' reading pretentious fiction.', theme.transition.typical,
                             ' writing articles and short stories.', theme.transition.typical,
                             ' learning about architecture and design.', theme.transition.typical,
-                            ' running.', theme.transition.typical,
+                            ' playing volleyball.', theme.transition.typical,
                         ]}
                         loop={Infinity}
                         wrapper={BodyTextSpan}
