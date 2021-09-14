a68f0bb6250bf645fe31c9e3585e4755a8493d48
Anna <anna.xing.22@gmail.com>
2021-09-14 09:27:01 -0400

    Update About pg for F21

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -22,26 +22,29 @@ export const AboutPage = ({theme}) => {
             </BannerContainer>
             <TextContainer>
                 <BodyText>
-                    I'm a second-year BCS student at the University of Waterloo. 
-	    	    I'm also currently a Portfolio Analytics Developer Intern at TD. 
+                    I'm a third-year BCS student at the University of Waterloo, graduating in April 2024.
 	            In my spare time, I volunteer as a Software Developer and Product Designer at Develop for Good, 
-	            where I'm building a full-stack web app for the educational nonprofit Classrooms Without Borders!
+	            where I'm building a full-stack web app for the educational nonprofit 
+	    	    <TextLink href="https://classroomswithoutborders.org/" target="_blank">Classrooms Without Borders</TextLink>!
+	    	    I'm also currently working on a complete website redesign for the climate nonprofit 
+	    	    </TextLink href="https://www.greenprobono.org/" target="_blank">Green Pro Bono</TextLink>.
 	    	</BodyText>
 	    	<BodyText>
-                    In the past, I've also been a Web Application Developer for UW's Computer Science Computing Facility,
-	            where I worked on applications supporting faculty and students. 
+                    In the past, I've interned as a Portfolio Analytics Developer at TD, as well as 
+	    	    a Web Application Developer for UW's Computer Science Computing Facility.
 	            I'm currently seeking opportunities for Winter 2022 in software development and UI/UX.
                 </BodyText>
                 <BodyText>
                     I'm passionate about human-centered design, which is why I'm pursuing a Human-Computer 
                     Interaction specialization and Cognitive Science minor within my degree. I love applying 
-	            interdisciplinary knowledge to design and build products for social good.
+	            interdisciplinary knowledge to design and build products for social and environmental good.
                 </BodyText>
                 <BodyText>
                     To chat about anything from software to Spanish basilicas, feel free to connect with 
                     me via email at
                     <TextLink href="mailto:anna.xing@uwaterloo.ca"> anna.xing@uwaterloo.ca </TextLink>
-                    or through any of my links below.
+                    or through any of my links below. 
+	    	    I particularly love talking about opportunities for social impact within tech!
                 </BodyText>
                 <BodyTextSpan>
                     When I'm not tinkering with dev tools, you can find me:
